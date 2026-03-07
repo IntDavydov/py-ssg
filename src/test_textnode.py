27b99f7c25c14655a36bd9b6b5456c0f5b8aae6f
davydov-lap <successful0506@gmail.com>
2026-03-07 20:31:46 +0200

    tests

---
+++ src/test_textnode.py
@@ -0,0 +1,49 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_neq(self):
+        node = TextNode("This is text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_nourl(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node.url, None)
+
+    def test_linktype(self):
+        node = TextNode("This is a text node", TextType.LINK)
+        self.assertEqual(node.text_type, TextType.LINK)
+
+    def test_imagetype(self):
+        node = TextNode("This is a text node", TextType.IMAGE)
+        self.assertEqual(node.text_type, TextType.IMAGE)
+
+    def test_italictype(self):
+        node = TextNode("This is a text node", TextType.ITALIC)
+        self.assertEqual(node.text_type, TextType.ITALIC)
+
+    def test_codetype(self):
+        node = TextNode("This is a text node", TextType.CODE)
+        self.assertEqual(node.text_type, TextType.CODE)
+
+    def test_textype(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        self.assertEqual(node.text_type, TextType.TEXT)
+
+    def test_repr(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        self.assertEqual(
+            "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -5,13 +5,13 @@ class TextType(Enum):
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
-    LINK = "links"
-    IMAGES = "images"
+    LINK = "link"
+    IMAGE = "image"
     TEXT = "text"
 
 
 class TextNode:
-    def __init__(self, text, text_type, url):
+    def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
